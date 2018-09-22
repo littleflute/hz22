eae0b227199cbe8bc1073d95a767f1e712c63a44
littleflute <13699175164@sina.cn>
2018-09-21 17:14:04 -0700

    Update t.js

--- tangshi/t.js
+++ tangshi/t.js
@@ -7,3 +7,25 @@ var b1		=  blo0.blBtn( divP ,"idBtn_play","play","blue");
 b1.onclick = function(){  p.play(); }
 var b2		=  blo0.blBtn( divP ,"idBtn_pause","pause","gray");
 b2.onclick = function(){  p.pause(); }
+
+var sTitle = "<a target= '_blank' href = 'https://github.com/littleflute/hz22/edit/master/tangshi/t.js'>t.js_v0.0. 12*</a>";
+var div4V = blo0.blDiv(divP,"div4V", sTitle  ,"gray");
+div4V.v = blo0.blDiv( div4V , div4V .id + "_v", "v", "gray");
+div4V.v1 = blo0.blDiv( div4V , div4V .id + "_v1", "v1", "gray");
+div4V.b1 = blo0.blBtn( div4V , div4V.id + "_b1", "startTimer" , "gray");
+div4V.b1.onclick = function(){
+	if( div4V.timer == null){
+		div4V.timer = setInterval(_ftn4Timer,100);
+		this.innerHTML = "stopTimer";
+	}
+	else{
+		clearInterval(div4V.timer); div4V.timer = null;
+		this.innerHTML = "startTimer" ;
+	}
+}
+function _ftn4Timer(){
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    div4V.v.innerHTML = t;	
+    div4V.v1.innerHTML = p.src;
+}
