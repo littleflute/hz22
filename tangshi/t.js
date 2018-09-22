62523122d6148b6d90e726b07f3865341401ecec
littleflute <13699175164@sina.cn>
2018-09-21 17:19:40 -0700

    Update t.js

--- tangshi/t.js
+++ tangshi/t.js
@@ -8,7 +8,10 @@ b1.onclick = function(){  p.play(); }
 var b2		=  blo0.blBtn( divP ,"idBtn_pause","pause","gray");
 b2.onclick = function(){  p.pause(); }
 
-var sTitle = "<a target= '_blank' href = 'https://github.com/littleflute/hz22/edit/master/tangshi/t.js'>t.js_v0.0. 12*</a>";
+var sTitle = "<a target= '_blank' href = 'https://github.com/littleflute/hz22/edit/master/tangshi/t.js'>t.js_v0.0. 14 *</a> : ";
+sTitle += "<a target='_blank' href = 'https://littleflute.github.io/hz22/tangshi/'> online </a> : ";
+sTitle += "<a target='_blank' href = 'https://littleflute.github.io/hz22/tangshi/t.js'> t.js </a>";
+
 var div4V = blo0.blDiv(divP,"div4V", sTitle  ,"gray");
 div4V.v = blo0.blDiv( div4V , div4V .id + "_v", "v", "gray");
 div4V.v1 = blo0.blDiv( div4V , div4V .id + "_v1", "v1", "gray");
