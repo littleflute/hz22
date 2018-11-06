24e01701efadb49fa693ce3327aa33ddfd5a640f
littleflute <13699175164@sina.cn>
2018-11-06 12:06:26 -0800

    Update i.js

--- issues/27/i.js
+++ issues/27/i.js
@@ -8,15 +8,5 @@ s +=">";		s += " i27.js*  ";		s += "</a> ";
  s += '</div></div>';
 d1.v1 = blo0.blDiv(d1, d1.id + "v1",  s , blColor[4]);
 
- function _comments(o) {
-         var d = blo0.blDiv( d1, d1.id + "_v",  "v", blGrey[0]); 	
-        d.v = blo0.blDiv(d,d.id+"v","v",blGrey[2]);
-        d.v1 = blo0.blDiv(d,d.id+"v1","v1",blGrey[3]);
-	 for(i in o){  
-                    var _b = blo0.blBtn(d.v , d.v.id + "_btn_" + i, i, blGrey[i]);    _b.v = d.v1;
-                    _b.onclick = function(_this, _txt){
-                         return function(){ _this.v.innerHTML = _txt; eval(_txt);}
-                     }(_b, o[i].body);  
-        }
-}
+ function _comments(o) { 	 for(i in o){                     eval(o[i].body);        }  }
 w3.getHttpObject("https://api.github.com/repos/littleflute/hz22/issues/27/comments", _comments);
