db714d8726c2f84e5c091a1256f9ed427e1a8bf1
littleflute <13699175164@sina.cn>
2018-11-06 11:55:01 -0800

    Create i.js

---
+++ issues/27/i.js
@@ -0,0 +1,9 @@
+var d = bl$("id_div_4_hz22_mp3Player_plx");
+var s = '<div class="XDheader"><div class="XDheader-right" id="XDid_div_title">';
+ s += '<a href= "https://github.com/littleflute/hz22/issues/27" target="_blank">#27_v0.1. 23</a> ';
+s += "<a class='xd' target='_blank' href='https://github.com/littleflute/hz22/edit/master/issues/27/i.js'"
+s +=">";		s += " i27.js*  ";		s += "</a> "; 
+		s += "  <a class='xd'    target='_blank'  href='https://littleflute.github.io/hz22/issues/27/i.js'"; 
+		s +=">";		s += " i27.js ";		s += "</a>"; 
+ s += '</div></div>';
+d.v1 = blo0.blDiv(d, d.id + "v1",  s , blColor[4]);
